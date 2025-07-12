38171754ac6733e830876b8fb7b77b84e19ccb61
koms <oms495@yahoo.com>
2025-07-11 20:38:22 -0700

    Initial Commit

---
+++ CAR ASSISTANT LOC/CAR_ASSISTANT_LOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAR_ASSISTANT_LOCApp.swift
+//  CAR ASSISTANT LOC
+//
+//  Created by K.OmsEngineering  on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CAR_ASSISTANT_LOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
